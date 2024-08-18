ba59c1a55d00515b4169ce8ef5eac626ab879345
Ronitkothari22 <ronitkothari22@gmail.com>
2024-08-18 07:36:01 +0530

    initial inalizations

---
+++ src/constants/index.js
@@ -0,0 +1,383 @@
+import {
+    flask,
+    farzi,
+    flutter,
+    python,
+    machinelearning,
+      mobile,
+      backend,
+      creator,
+      web,
+      javascript,
+      typescript,
+      html,
+      css,
+      reactjs,
+      redux,
+      tailwind,
+      nodejs,
+      mongodb,
+      git,
+      figma,
+      docker,
+      meta,
+      starbucks,
+      tesla,
+      shopify,
+      clumoss,
+      topialifescience,
+      carrent,
+      jobit,
+      tripguide,
+      threejs,
+      attendx,
+    } from "../assets";
+    
+    export const navLinks = [
+      {
+        id: "about",
+        title: "About",
+      },
+  
+      {
+        id: "work",
+        title: "Work",
+      },
+      {
+        id: "skills",
+        title: "Skills",
+      },
+      {
+        id: "projects",
+        title: "Projects",
+      },
+      {
+        id: "contact",
+        title: "Contact",
+      },
+    ];
+    
+    const services = [
+      {
+        title: "Web Developer",
+        icon: web,
+      },
+      {
+        title: "Flutter Developer",
+        icon: mobile,
+      },
+      {
+        title: "Backend Developer",
+        icon: backend,
+      },
+      {
+        title: "AI/ML Developer",
+        icon: creator,
+      },
+    ];
+    
+    const technologies = [
+      {
+        name: "HTML 5",
+        icon: html,
+      },
+      {
+        name: "Flask",
+        icon: flask,
+      },
+      {
+        name: "CSS 3",
+        icon: css,
+      },
+      {
+        name: "JavaScript",
+        icon: javascript,
+      },
+      {
+        name: "Python",
+        icon: python,
+      },
+      {
+        name: "React JS",
+        icon: reactjs,
+      },
+      {
+        name: "Redux Toolkit",
+        icon: redux,
+      },
+      {
+        name: "Tailwind CSS",
+        icon: tailwind,
+      },
+      {
+        name: "Node JS",
+        icon: nodejs,
+      },
+      {
+        name: "MongoDB",
+        icon: mongodb,
+      },
+      {
+        name: "Three JS",
+        icon: threejs,
+      },
+      {
+        name: "git",
+        icon: git,
+      },
+      {
+        name: "Flutter",
+        icon: flutter,
+      },
+      {
+        name: "Machine learning",
+        icon: machinelearning,
+      },
+    ];
+    
+    // const experiences = [
+    //   {
+    //     title: "React.js Developer",
+    //     company_name: "Starbucks",
+    //     icon: starbucks,
+    //     iconBg: "#383E56",
+    //     date: "March 2020 - April 2021",
+    //     points: [
+    //       "Developing and maintaining web applications using React.js and other related technologies.",
+    //       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //       "Implementing responsive design and ensuring cross-browser compatibility.",
+    //       "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    //   },
+    //   {
+    //     title: "React Native Developer",
+    //     company_name: "Tesla",
+    //     icon: tesla,
+    //     iconBg: "#E6DEDD",
+    //     date: "Jan 2021 - Feb 2022",
+    //     points: [
+    //       "Developing and maintaining web applications using React.js and other related technologies.",
+    //       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //       "Implementing responsive design and ensuring cross-browser compatibility.",
+    //       "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    //   },
+    //   {
+    //     title: "Web Developer",
+    //     company_name: "Shopify",
+    //     icon: shopify,
+    //     iconBg: "#383E56",
+    //     date: "Jan 2022 - Jan 2023",
+    //     points: [
+    //       "Developing and maintaining web applications using React.js and other related technologies.",
+    //       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //       "Implementing responsive design and ensuring cross-browser compatibility.",
+    //       "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    //   },
+    //   {
+    //     title: "Full stack Developer",
+    //     company_name: "Meta",
+    //     icon: meta,
+    //     iconBg: "#E6DEDD",
+    //     date: "Jan 2023 - Present",
+    //     points: [
+    //       "Developing and maintaining web applications using React.js and other related technologies.",
+    //       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //       "Implementing responsive design and ensuring cross-browser compatibility.",
+    //       "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    //   },
+    // ];
+    
+  
+    const experiences = [
+      {
+        title: "AI/ML Intern",
+        company_name: "CLUMOSS",
+        icon: clumoss, // You'll need to replace this with the appropriate icon
+        iconBg: "#383E56",
+        date: "Aug 2023 - Sept 2023",
+        points: [
+          "Completed a comprehensive internship on machine learning algorithms.",
+          "Collaborated with a team to solve complex problems.",
+        ],
+      },
+      {
+        title: "Data Science Intern",
+        company_name: "TopiaLife Science",
+        icon: topialifescience, // You'll need to replace this with the appropriate icon
+        iconBg: "#E6DEDD",
+        date: "May 2023 - June 2023",
+        points: [
+          "Developed algorithms to automate resume processing.",
+          "Utilized NLP to extract relevant information.",
+          "Collaborated with HR team to meet their requirements.",
+        ],
+      },
+    ];
+  
+  
+    const testimonials = [
+      {
+        testimonial:
+          "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        name: "Sara Lee",
+        designation: "CFO",
+        company: "Acme Co",
+        image: "https://randomuser.me/api/portraits/women/4.jpg",
+      },
+      {
+        testimonial:
+          "I've never met a web developer who truly cares about their clients' success like Rick does.",
+        name: "Chris Brown",
+        designation: "COO",
+        company: "DEF Corp",
+        image: "https://randomuser.me/api/portraits/men/5.jpg",
+      },
+      {
+        testimonial:
+          "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        name: "Lisa Wang",
+        designation: "CTO",
+        company: "456 Enterprises",
+        image: "https://randomuser.me/api/portraits/women/6.jpg",
+      },
+    ];
+    
+    // const projects = [
+    //   {
+    //     name: "Car Rent",
+    //     description:
+    //       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    //     tags: [
+    //       {
+    //         name: "react",
+    //         color: "blue-text-gradient",
+    //       },
+    //       {
+    //         name: "mongodb",
+    //         color: "green-text-gradient",
+    //       },
+    //       {
+    //         name: "tailwind",
+    //         color: "pink-text-gradient",
+    //       },
+    //     ],
+    //     image: carrent,
+    //     source_code_link: "https://github.com/",
+    //   },
+    //   {
+    //     name: "Job IT",
+    //     description:
+    //       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    //     tags: [
+    //       {
+    //         name: "react",
+    //         color: "blue-text-gradient",
+    //       },
+    //       {
+    //         name: "restapi",
+    //         color: "green-text-gradient",
+    //       },
+    //       {
+    //         name: "scss",
+    //         color: "pink-text-gradient",
+    //       },
+    //     ],
+    //     image: jobit,
+    //     source_code_link: "https://github.com/",
+    //   },
+    //   {
+    //     name: "Trip Guide",
+    //     description:
+    //       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    //     tags: [
+    //       {
+    //         name: "nextjs",
+    //         color: "blue-text-gradient",
+    //       },
+    //       {
+    //         name: "supabase",
+    //         color: "green-text-gradient",
+    //       },
+    //       {
+    //         name: "css",
+    //         color: "pink-text-gradient",
+    //       },
+    //     ],
+    //     image: tripguide,
+    //     source_code_link: "https://github.com/",
+    //   },
+    // ];
+  
+  
+  
+  
+    const projects = [
+      {
+        name: "AttendX",
+        description:
+          "A WiFi-based attendance system that integrates React Native frontend with Django ML backend, enhancing app intelligence and accessibility.",
+        tags: [
+          {
+            name: "react-native",
+            color: "blue-text-gradient",
+          },
+          {
+            name: "django",
+            color: "green-text-gradient",
+          },
+          {
+            name: "machine-learning",
+            color: "pink-text-gradient",
+          },
+        ],
+        image: attendx, // You'll need to import this image
+        source_code_link: "https://github.com/ProdigyRahul/AttendX", // Replace with actual link
+      },
+      {
+        name: "Inventory Management System",
+        description:
+          "A robust system enabling real-time stock updates and efficient inventory tracking with an automated alert system for stock level notifications.",
+        tags: [
+          {
+            name: "python",
+            color: "blue-text-gradient",
+          },
+          {
+            name: "database",
+            color: "green-text-gradient",
+          },
+          {
+            name: "alert-system",
+            color: "pink-text-gradient",
+          },
+        ],
+        image: tripguide, // You'll need to import this image
+        source_code_link: "https://github.com/", // Replace with actual link
+      },
+      {
+        name: "Farzi: Fake News Detector",
+        description:
+          "A precise binary classification deep learning model with 91% accuracy for detecting fake news, incorporating web scraping to gather data from APIs.",
+        tags: [
+          {
+            name: "deep-learning",
+            color: "blue-text-gradient",
+          },
+          {
+            name: "nlp",
+            color: "green-text-gradient",
+          },
+          {
+            name: "web-scraping",
+            color: "pink-text-gradient",
+          },
+        ],
+        image: farzi, // You'll need to import this image
+        source_code_link: "https://github.com/Ronitkothari22/Fake-news-detetcor", // Replace with actual link
+      },
+    ];
+    
+    export { services, technologies, experiences, testimonials, projects };
